d66f1f2ba041fd6f8d3a3819f7726362511c79de
Ernie <unknowninjah@gmail.com>
2018-07-17 06:56:46 -0400

    menu update

--- menu.sh
+++ menu.sh
@@ -10,7 +10,7 @@ echo =============================
 PS3='Enter Choice: '
 COLUMNS=12
 
-options=("Scan Drives" "Make Re-Block files" "Remove Re-Block files" "Run" "Quit")
+options=("Scan Drives" "Make Re-Block files" "Remove Re-Block files" "Run" "Status" "Quit")
 
 select opt in "${options[@]}"
 do
@@ -18,44 +18,60 @@ case $opt in
 "Scan Drives")
 	clear
 	echo =============================
-	sg_scan | awk '{print $1}'|grep -v 'sg0:\|sg1:\|sg2:'
+        echo -n "Drive's Detected: "
+	sg_scan | awk '{print $1}'|grep -v 'sg0:\|sg1:\|sg2:' -c
 	echo =============================
+	sleep .5
 	./menu.sh
 	;;
 
 "Make Re-Block files")
 	clear
-	for d in {2..99}
+	for d in {3..99}
 	do
-	#echo sg_format --format --size=512 /dev/sg$d >> hdd$d.sh
-	echo echo test $d of 99 >> hdd$d.sh
-	chmod +x hdd$d.sh
+	echo sg_format --format --size=512 /dev/sg$d >> hdd$d.sh
+	echo "./hdd$d.sh 2>/dev/null &" >> start_format.sh
+	chmod +x hdd*
+	chmod +x start_format.sh
 	done
+	clear
 	echo "Re-Block files have been created."
 	./menu.sh
 	;;
 
 "Remove Re-Block files")
 	clear
-	for d in {2..99}
+	for d in {3..99}
 	do
 	rm -rf hdd$d.sh
+	rm -rf start_format.sh  
 	done
+	clear
 	echo "Re-Block files have been removed."
 	./menu.sh
 	;;
 
-"Run")
-        for d in {2..99}
+"Run")	
+	clear
+	./start_format.sh
+	sleep 1
+	;;
+
+"Status")
+        for d in {3..99}
 	do
-	./hdd$d.sh 2>/dev/null
+	if
+	ps -a |grep hdd$d.sh --silent
+	then 
+	echo "Drive $d is still in progress."
+	fi
 	done
 	;;
 
 "Quit")
 	clear
+	sleep .5
 	break
-	exit
 	;;
 
 *)
