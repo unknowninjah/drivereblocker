ea76b9be68be66e31dba41753b3569ce4e9800bf
unknowninjah <34119687+unknowninjah@users.noreply.github.com>
2018-07-23 16:07:00 -0700

    Create badblock_menu.sh

---
+++ badblock_menu.sh
@@ -0,0 +1,12 @@
+clear
+for d in {0..99}
+do
+echo badblocks -vns -e10 -o HDD$d.txt /dev/sg$d
+echo "./HDD$d.sh 2>/dev/null &" >> start_block_check.sh
+done
+chmod +x HDD*
+chmod +x start_block_check.sh
+clear
+echo ""
+echo "Script files have been created, ready for Block Check."
+echo ""
