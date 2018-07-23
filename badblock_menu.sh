6da3a77c59eff9b28bf0b5d6ff2aa3875547a431
unknowninjah <34119687+unknowninjah@users.noreply.github.com>
2018-07-23 16:11:32 -0700

    Update badblock_menu.sh

--- badblock_menu.sh
+++ badblock_menu.sh
@@ -1,7 +1,7 @@
 clear
 for d in {0..99}
 do
-echo badblocks -vns -e10 -o HDD$d.txt /dev/sg$d
+echo badblocks -vns -e10 -o HDD$d.txt /dev/sg$d >> HDD$d.sh
 echo "./HDD$d.sh 2>/dev/null &" >> start_block_check.sh
 done
 chmod +x HDD*
