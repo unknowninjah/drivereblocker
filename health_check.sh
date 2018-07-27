a2b4fe391c2214b4ca14d4cf5ae5adc06b4ea0ef
unknowninjah <34119687+unknowninjah@users.noreply.github.com>
2018-07-27 15:18:07 -0700

    Update health_check.sh

--- health_check.sh
+++ health_check.sh
@@ -1,4 +1,3 @@
-today=$(date +%m-%d-%Y)
 clear
 echo -n "Checking drive health..."
 
@@ -21,16 +20,16 @@ else
 if
 smartctl -i /dev/sg$i 2>/dev/null |grep 'Vendor:\|Serial number:\|SMART\|Logical block size:\|Product:' |grep failed --silent
 then
-echo "                      Drive $i Failed" >> $today.log 
-smartctl -a /dev/sg$i 2>/dev/null |grep 'Vendor:\|Serial number:\|Logical block size:' >> $today.log && echo "=====================================================" >> $today.log
+echo "                      Drive $i Failed" >> TheDrives.log 
+smartctl -a /dev/sg$i 2>/dev/null |grep 'Vendor:\|Serial number:\|Logical block size:' >> TheDrives.log && echo "=====================================================" >> TheDrives.log
 else
-smartctl -a /dev/sg$i 2>/dev/null |grep 'User Capacity\|Product:\|Vendor:\|Logical block size\|Form Factor\|Serial number\|SMART Health Status\|Non-medium error count' >> $today.log 
+smartctl -a /dev/sg$i 2>/dev/null |grep 'User Capacity\|Product:\|Vendor:\|Logical block size\|Form Factor\|Serial number\|SMART Health Status\|Non-medium error count' >> TheDrives.log 
 fi
 
 if
 smartctl -i /dev/sg$i |grep ' INFORMATION SECTION' --silent
 then
-echo "=====================================================" >> $today.log
+echo "=====================================================" >> TheDrives.log
 fi
 
 fi 
@@ -44,4 +43,4 @@ echo " "
 echo "Drive Status: "
 echo "====================================================="
 
-cat $today.log
+cat TheDrives.log
